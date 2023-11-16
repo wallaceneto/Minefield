54f3812fc8e1646d313e3fc8601c28130582e801
Wallace Neto <wallace.neto@outlook.com>
2023-11-16 20:44:32 -0300

    feat: creating board functions

---
+++ src/global/functions.ts
@@ -0,0 +1,47 @@
+const createBoard = (rows: number, columns: number) => {
+  return Array(rows)
+    .fill(0)
+    .map((_, row) => {
+      return Array(columns)
+        .fill(0)
+        .map((_, column) => {
+          return {
+            row,
+            column,
+            opened: false,
+            flagged: false,
+            mined: false,
+            exploded: false,
+            nearMines: 0,
+          };
+        });
+    });
+};
+
+const spreadMines = (board: any, minesAmount: number) => {
+  const rows = board.length;
+  const columns = board[0].length;
+  let minesPlanted = 0;
+
+  while (minesPlanted < minesAmount) {
+    const rowSel = Math.random() * rows;
+    const columnSel = Math.random() * columns;
+
+    if (!board[rowSel][columnSel].mined) {
+      board[rowSel][columnSel].mined = true;
+      minesPlanted++;
+    }
+  }
+};
+
+const createMineBoard = (
+  rows: number,
+  columns: number,
+  minesAmount: number,
+) => {
+  const board = createBoard(rows, columns);
+  spreadMines(board, minesAmount);
+  return board;
+};
+
+export {createMineBoard};
